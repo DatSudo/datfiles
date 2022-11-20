ae9a5679af947c28fc19e4e44e8d4e2c114ad6f6
Datsudo <ekflfdv29@gmail.com>
2022-11-20 16:24:04 +0800

    dot backup: 2022-11-20 | 16:24:04

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+bar="main"
+
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar $bar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
 
